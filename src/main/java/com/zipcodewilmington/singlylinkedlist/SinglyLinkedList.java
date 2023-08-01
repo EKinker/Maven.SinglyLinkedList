ae70bd9a1f98ce36506339139d48763fac899737
Liz Kinker <Liz.Kinker@gmail.com>
2023-08-01 16:00:47 -0400

    Started Node

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,23 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    class Node{
+
+        int value;
+        Node next;
+
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+        public int getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+    }
+
 }
